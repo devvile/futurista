d301dd19d8d3bfe4196e4682114407038be34244
devvile <patryk.czemierowski@gmail.com>
2025-10-04 17:43:21 +0200

    headaer fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Header } from "@/components/Header"
+import  Header  from "@/components/header"
 import { Bebas_Neue } from 'next/font/google'
 
 export const bebasNeue = Bebas_Neue({ 
--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { Logo } from "./logo";
 import { MobileMenu } from "./mobile-menu";
 
-export const Header = () => {
+const Header = () => {
   return (
     <div className="absolute z-50 pt-8 md:pt-14 top-0 left-0 w-full">
       <header className="flex items-center justify-between container">
@@ -25,3 +25,5 @@ export const Header = () => {
     </div>
   );
 };
+
+export default Header;
