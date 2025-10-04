a922e2c3d92147d42109ec81696e8c01af31e20e
devvile <patryk.czemierowski@gmail.com>
2025-10-04 17:46:43 +0200

    tailwindfix

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        border: 'hsl(var(--border))',
+      },
+      fontFamily: {
+        sentient: ['Sentient', 'sans-serif'],
+      },
+      boxShadow: {
+        glow: '0 0 8px 2px var(--tw-shadow-color)',
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
