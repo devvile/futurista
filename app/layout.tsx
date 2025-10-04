14541bfe0a693aa1c7f2fd9d5410e3ed720d9fb9
devvile <patryk.czemierowski@gmail.com>
2025-10-04 14:12:33 +0200

    init

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Header } from "@/components/header";
+import { Bebas_Neue } from 'next/font/google'
+
+export const bebasNeue = Bebas_Neue({ 
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-bebas-neue', // CSS variable
+})
+
+export const metadata: Metadata = {
+  title: "Futurista - Ai solution for your business",
+  description: "Investment strategies that outperform the market",
+    generator: 'v0.app'
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${bebasNeue.variable} antialiased`}
+        suppressHydrationWarning
+      >
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/hero.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import { GL } from "./gl";
+import { Pill } from "./pill";
+import { Button } from "./ui/button";
+import { useState } from "react";
+
+export function Hero() {
+  const [hovering, setHovering] = useState(false);
+  return (
+    <div className="flex flex-col h-svh justify-between">
+      <GL hovering={hovering} />
+
+      <div className="pb-16 mt-auto text-center relative">
+        <h1 className="text-5xl sm:text-6xl md:text-7xl font-sentient">
+          Defining the Future. <br />
+          <i className="font-light">Now.</i> 
+        </h1>
+        <p className="font-mono text-sm sm:text-base text-foreground/60 text-balance mt-8 max-w-[440px] mx-auto">
+          Through perpetual investment strategies that outperform the market
+        </p>
+
+        <Link className="contents max-sm:hidden" href="/#contact">
+          <Button
+            className="mt-14"
+            onMouseEnter={() => setHovering(true)}
+            onMouseLeave={() => setHovering(false)}
+          >
+            Contact Us
+          </Button>
+        </Link>
+        <Link className="contents sm:hidden" href="/#contact">
+          <Button
+            size="sm"
+            className="mt-14"
+            onMouseEnter={() => setHovering(true)}
+            onMouseLeave={() => setHovering(false)}
+          >
+            [Contacts Us]
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
