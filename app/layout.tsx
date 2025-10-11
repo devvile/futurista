0e468a3aaa303af191c5da4391f9046111de0db4
devvile <patryk.czemierowski@gmail.com>
2025-10-11 13:13:34 +0200

    ganalytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,17 @@ import type { Metadata } from "next";
 import "./globals.css";
 import  Header  from "@/components/header"
 import { Bebas_Neue } from 'next/font/google'
+import Script from 'next/script'
 
 export const bebasNeue = Bebas_Neue({ 
   subsets: ['latin'],
   weight: '400',
-  variable: '--font-bebas-neue', // CSS variable
+  variable: '--font-bebas-neue', 
 })
 
 export const metadata: Metadata = {
   title: "Futurista - Ai solution for your business",
-  description: "",
+  description: "Automations, Ai Integrations, Ai Agents, Applications, Mobile App, Development, Software",
 };
 
 export default function RootLayout({
@@ -22,6 +23,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+            <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-NFMV5L8X4F"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NFMV5L8X4F');
+          `}
+        </Script>
+      </head>
       <body
         className={`${bebasNeue.variable} antialiased`}
         suppressHydrationWarning
